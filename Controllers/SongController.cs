c6062e727e2839b716feba8f9742cd67db020b61
AndreyEliVar <andrey.elizondovargas@ucr.ac.cr>
2022-05-03 21:37:11 -0600

    Entrega de laboratorio 3 Ingenieria de software

---
+++ Controllers/SongController.cs
@@ -0,0 +1,24 @@
+﻿using Laboratorio3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Laboratorio3.Controllers
+{
+    public class SongController : Controller
+    {
+        public IActionResult Index()
+        {
+            var song = GetFavoriteSong();
+            ViewBag.MainTitle = "";
+            return View(song);
+        }
+
+        private SongModel GetFavoriteSong()
+        {
+            var song = new SongModel  { Country = "Korea", Name = "Can't Control Myself", Genre = "K-Pop", Interpreter = "Taeyeon", ReleaseDate = new DateTime(2022, 10, 26) };
+
+            return song;
+        }
+    }
+}
---
+++ Models/SongModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Laboratorio3.Models
+{
+    public class SongModel
+    {
+        public string Country { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Interpreter { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
